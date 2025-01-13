7a0926a5c3f680a0db60407c67837e2b6639e316
dillacorn <28743863+dillacorn@users.noreply.github.com>
2025-01-13 15:58:15 -0500

    Update 16.10_stretched_res.bat

--- UserProfile/scripts/16.10_stretched_res.bat
+++ UserProfile/scripts/16.10_stretched_res.bat
@@ -8,10 +8,12 @@ REM - Adjust display refresh rate: "240"
 
 REM - Native Display Resolution: 1920x1080 (FHD)
 nircmd setdisplay 1600 1024 32 400
-REM - nircmd setdisplay 1728 1080 32 400
+REM nircmd setdisplay 1728 1080 32 400
 
 REM - Native Display Resolution: 2560x1440 (QHD)
 REM nircmd setdisplay 2304 1440 32 240
+REM nircmd setdisplay 2048 1280 32 240  REM - Step-down QHD resolution (16:10)
 
 REM - Native Display Resolution: 3840x2160 (UHD)
 REM nircmd setdisplay 3456 2160 32 120
+REM nircmd setdisplay 3072 1920 32 120  REM - Step-down UHD resolution (16:10)
