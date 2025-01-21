28e160b89effc9b351d6d3cc630e1d0a66142c32
dillacorn <28743863+dillacorn@users.noreply.github.com>
2025-01-21 10:15:55 -0500

    added another popular CS2 and Valorant res.

---
+++ UserProfile/scripts/1680x1050_stretched_res.bat
@@ -0,0 +1,9 @@
+@echo off
+
+REM - https://calculateaspectratio.com/ ~ How I got resolution calculations
+
+REM - Remove "REM" to use command when running ".bat"
+REM - Adjust color depth: "32"
+REM - Adjust display refresh rate: "240"
+
+nircmd setdisplay 1680 1050 32 400
