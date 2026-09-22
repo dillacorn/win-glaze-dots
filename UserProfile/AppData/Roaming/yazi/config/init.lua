96e95fbc741d2f3cfd09186006e71def959692e4
dillacorn <28743863+dillacorn@users.noreply.github.com>
2026-09-22 15:45:28 -0400

    Match stable Yazi linemode API

--- UserProfile/AppData/Roaming/yazi/config/init.lua
+++ UserProfile/AppData/Roaming/yazi/config/init.lua
@@ -12,7 +12,7 @@ function Linemode:size_and_mtime()
         size_text = folder and tostring(#folder.files) or "-"
     end
 
-    local time = math.floor(self._file.stat.mtime or 0)
+    local time = math.floor(self._file.cha.mtime or 0)
     local date_text = "-"
 
     if time > 0 then
