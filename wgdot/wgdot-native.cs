504477b19849dfd9b5fc60ed799f2627615d3f79
dillacorn <28743863+dillacorn@users.noreply.github.com>
2026-09-21 22:17:38 -0400

    Bump stabilization runtime preview

--- wgdot/wgdot-native.cs
+++ wgdot/wgdot-native.cs
@@ -21,7 +21,7 @@ using Microsoft.Win32;
 
 internal static class WgdotNative
 {
-    const string Version = "native-preview-69";
+    const string Version = "native-preview-70";
     const int WingetPreflightTimeoutMs = 30000;
     const string RepoFullName = "dillacorn/win-glaze-dots";
     const string RepoUrl = "https://github.com/dillacorn/win-glaze-dots.git";
